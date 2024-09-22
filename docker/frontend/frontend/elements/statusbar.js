da6d7031b699710cb38d843a9e5371bc95f1bd15
usq33871 <tibor.auer@rfi.ac.uk>
2024-09-22 23:23:07 +0000

    REFACTOR - statusbar extends div

--- docker/frontend/frontend/elements/statusbar.js
+++ docker/frontend/frontend/elements/statusbar.js
@@ -1,30 +1,31 @@
-class StatusBar {
+class StatusBar extends HTMLDivElement {
     constructor(initialType, initialMessage) {
+        super();
+
         if (sessionStorage.getItem("statusBar")) {
             let sb = JSON.parse(sessionStorage.getItem("statusBar"));
             initialType = sb.type;
             initialMessage = sb.message;
         }          
 
-        const statusBar = document.createElement("div")
-        statusBar.className = "status";
-        document.body.appendChild(statusBar);
-        this.statusBar = statusBar;
+        this.className = "status";
+        document.body.appendChild(this);
 
-        const statusTxt = document.createElement("p");
-        this.statusBar.appendChild(statusTxt);
-        this.statusTxt = statusTxt;
+        this.statusTxt = document.createElement("p");
+        this.appendChild(this.statusTxt);
 
         this.message(initialType, initialMessage);
     }
 
     message(type, message) {
-        this.statusBar.className = `status ${type}`;
+        this.className = `status ${type}`;
         if (!type) { type = "Status" }
         this.statusTxt.innerHTML = `<strong>${type}</strong> - ${message}`;
         sessionStorage.setItem("statusBar",JSON.stringify({type: type, message: message}));
       }
       
 }
+
+window.customElements.define('status-bar', StatusBar, {extends: "div"})
 
 export { StatusBar }
